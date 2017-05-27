dd28563e259f963211d076f1440075b46d43635a
Yunhan <yunhanyu@usc.edu>
2017-05-26 22:37:09 -0700

    second

---
+++ script2.py
@@ -0,0 +1,9 @@
+import random, string
+
+def generator():
+    letter1 = random.choice(string.ascii_lowercase);
+    letter2 = random.choice(string.ascii_lowercase);
+    letter3 = random.choice(string.ascii_lowercase);
+    return(name);
+
+print(generator())
